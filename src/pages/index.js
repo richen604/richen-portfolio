f79d456961b3c4e7f8dc09e0a9eb7a9382eb31dc
richen604 <56615615+richen604@users.noreply.github.com>
2021-04-05 07:38:40 -0700

    set up eslint and fixed linter errors. added reactstrap and react-spring

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>I am building something great</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -21,7 +21,7 @@ const IndexPage = () => (
     />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <Link to="/using-typescript/">Go to &quot;Using TypeScript&quot;</Link>
     </p>
   </Layout>
 )
