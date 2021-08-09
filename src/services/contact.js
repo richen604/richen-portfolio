79accec353c03fe107f013897925d807e09d6bbf
richen604 <56615615+richen604@users.noreply.github.com>
2021-08-08 17:49:26 -0700

    clean repo for merge of next-migrate

--- src/services/contact.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-export const postContact = async values => {
-  console.log(values)
-  const result = await axios
-    .post("https://richen-emailer.herokuapp.com/contact", values)
-    .catch(error => console.log(error))
-  return result
-}
